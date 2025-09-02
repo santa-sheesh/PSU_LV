83e3b2e23627e7379ec442ba4d78ff5b8318be3d
santa-sheesh <134328127+santa-sheesh@users.noreply.github.com>
2025-09-02 13:13:14 +0200

    Create 3.py

---
+++ LV_5/3.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+df = pd.read_csv("occupancy_processed.csv")
+
+X = df[["S3_Temp", "S5_CO2"]]
+y = df["Room_Occupancy_Count"]
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+dt = DecisionTreeClassifier()
+dt.fit(x_train, y_train)
+y_pred = dt.predict(x_test)
+
+print(classification_report(y_test, y_pred))
+
+plt.figure(figsize=(12, 8))
+plot_tree(dt, filled=True, feature_names=X.columns, class_names=["Slobodna", "Zauzeta"])
+plt.show()
+
+cm = confusion_matrix(y_test, y_pred)
+ConfusionMatrixDisplay(cm, display_labels=["Slobodna", "Zauzeta"]).plot(cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.show()
